2ba6ea58b8c55f0aefbb381900667eb346313777
Viacheslav Konstantinov <vyacheslav.konstantinov@phystech.edu>
2019-10-20 23:01:22 +0300

    Update testParser.cpp

--- 01/testParser.cpp
+++ 01/testParser.cpp
@@ -6,8 +6,8 @@
 #include <cstring>
 using namespace Catch::literals;
 
+// function to get output of parser from command line
 int GetStdoutFromCommand(std::string cmd) {
-
     std::string data;
     FILE * stream;
     const int max_buffer = 256;
@@ -16,35 +16,31 @@ int GetStdoutFromCommand(std::string cmd) {
     stream = popen(cmd.c_str(), "r");
     if (stream) {
         while (!feof(stream))
-        if (fgets(buffer, max_buffer, stream) != NULL) 
-            data.append(buffer);
-        pclose(stream);
-    }
+            if (fgets(buffer, max_buffer, stream) != NULL)
+                data.append(buffer);
+        }    
+    pclose(stream);   
     return std::stoi(data);
 }
 
 TEST_CASE( "Arithmetic expressions", "[classArthParser]" ) {
+    int res = 85-9*7+99/11;
+    std::string cmd = "./arithmeticParser 85-9*7+99/11";
+    REQUIRE(GetStdoutFromCommand(cmd) == res);
 
-    SECTION( "Checking accuracy of computation" ) {
-        
-        int res = 85-9*7+99/11;
-        std::string cmd = "./arithmeticParser 85-9*7+99/11";
-        REQUIRE(GetStdoutFromCommand(cmd) == res);
+    res = -12*4-98-19/9;
+    cmd = "./arithmeticParser -12*4-98-19/9";
+    REQUIRE(GetStdoutFromCommand(cmd) == res);
 
-        res = -12*4-98-19/9;
-        cmd = "./arithmeticParser -12*4-98-19/9";
-        REQUIRE(GetStdoutFromCommand(cmd) == res);
+    res = (-74)*2+148-8+27/3;
+    cmd = "./arithmeticParser -74*2+148-8+27/3";
+    REQUIRE(GetStdoutFromCommand(cmd) == res);
 
-        res = (-74)*2+148-8+27/3;
-        cmd = "./arithmeticParser -74*2+148-8+27/3";
-        REQUIRE(GetStdoutFromCommand(cmd) == res);
+    res = 144/(-4)-(-8)+4*(-5);
+    cmd = "./arithmeticParser 144/-4--8+4*-5";
+    REQUIRE(GetStdoutFromCommand(cmd) == res);
 
-        res = 144/(-4)-(-8)+4*(-5);
-        cmd = "./arithmeticParser 144/-4--8+4*-5";
-        REQUIRE(GetStdoutFromCommand(cmd) == res);
-
-        res = 78;
-        cmd = "./arithmeticParser 78";
-        REQUIRE(GetStdoutFromCommand(cmd) == res);
-    }
+    res = 78;
+    cmd = "./arithmeticParser 78";
+    REQUIRE(GetStdoutFromCommand(cmd) == res);
 }
