94de4ff801e5faf4bc8efa1fcf467058771b76fe
Viacheslav Konstantinov <vyacheslav.konstantinov@phystech.edu>
2020-01-08 21:01:52 +0300

    Update test2ArgIdxOutOfRange.cpp

--- 07/test2ArgIdxOutOfRange.cpp
+++ 07/test2ArgIdxOutOfRange.cpp
@@ -10,8 +10,10 @@ int main()
     bool myBool = false;
     std::string myStr = "false";
 
+    std::string expr1 = "Expression 1: {0}{1}/{2} = {55} is also {4}";
+
     std::cout << "-------------------------------------------------------------------\n";
     std::cout << "Example of an error of an argument index out of range:\n";
-    std::cout << pythonString("Expression 1: {0}{1 }/{2} = {55} is also {4}", x, y, 3, 9, myStr) << std::endl;
+    std::cout << pythonString(expr1, x, y, 3, 9, myStr) << std::endl;
     return 0;
 }
