9f33848b3970884aafa7037f88cdeafa436033ad
Viacheslav Konstantinov <vyacheslav.konstantinov@phystech.edu>
2020-01-08 21:03:04 +0300

    Update test4NotIntegerInBraces.cpp

--- 07/test4NotIntegerInBraces.cpp
+++ 07/test4NotIntegerInBraces.cpp
@@ -9,8 +9,10 @@ int main()
     bool myBool = false;
     std::string myStr = "false";
 
+    std::string expr1 = "Expression 1: {3} {1} / {0} + ({1} - {0}) = {2} is {4} ({y})";
+
     std::cout << "-------------------------------------------------------------------\n";
     std::cout << "Example of an error of non integer within braces:\n";
-    std::cout << pythonString("Expression 1: {3} {1} / {0} + ({1} - {0}) = {2} is {4} ({y})", x, y, z, "Expression", myBool, myStr) << std::endl;
+    std::cout << pythonString(expr1, x, y, z, "Expression", myBool, myStr) << std::endl;
     return 0;
 }
