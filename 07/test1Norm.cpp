3e18507b33802b29f2c7dc2eebf65ce5d64c563b
Viacheslav Konstantinov <vyacheslav.konstantinov@phystech.edu>
2020-01-08 20:59:23 +0300

    Update test1Norm.cpp

--- 07/test1Norm.cpp
+++ 07/test1Norm.cpp
@@ -9,11 +9,15 @@ int main()
     double z = 4.5;
     bool myBool = false;
     std::string myStr = "false";
-
+    std::string expr1, expr2;
+    
+    expr1 = "Expression 1: {3} {1} / {0} + ({1} - {0}) = {2} is {4} ({5})";
+    expr2 = "Expression 2: {0}{1}/{2} = {3} is also {4}";
+    
     std::cout << "-------------------------------------------------------------------\n";
     std::cout << "Example of using of the pythonString function without errors:\n";
-    std::cout << pythonString("Expression 1: {3} {1} / {0} + ({1} - {0}) = {2} is {4} ({5})", x, y, z, "Expression", myBool, myStr) << std::endl;
-    std::cout << pythonString("Expression 2: {0}{1 }/{2} = {3} is also {4}", x, y, 3, 9, myStr) << std::endl;
+    std::cout << pythonString(expr1, x, y, z, "Expression", myBool, myStr) << std::endl;
+    std::cout << pythonString(expr2, x, y, 3, 9, myStr) << std::endl;
     std::cout << "-------------------------------------------------------------------\n";
     return 0;
 }
