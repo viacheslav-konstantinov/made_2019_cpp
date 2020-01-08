cdc941e34d14a36d56f5c192e56a175267d329d1
Viacheslav Konstantinov <vyacheslav.konstantinov@phystech.edu>
2020-01-08 21:02:26 +0300

    Update test3BraceOutOfContext.cpp

--- 07/test3BraceOutOfContext.cpp
+++ 07/test3BraceOutOfContext.cpp
@@ -7,8 +7,10 @@ int main()
     int y = 5;
     std::string myStr = "false";
 
+    std::string expr1 = "Expression 1: {0}{1}/{2} = {3} { is also {4}";
+
     std::cout << "-------------------------------------------------------------------\n";
     std::cout << "Example of an error of using a brace out of context:\n";
-    std::cout << pythonString("Expression 1: {0}{1 }/{2} = {3} { is also {4}", x, y, 3, 9, myStr) << std::endl;
+    std::cout << pythonString(expr1, x, y, 3, 9, myStr) << std::endl;
     return 0;
 }
