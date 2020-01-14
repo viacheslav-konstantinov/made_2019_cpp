44aafcf7fff17a15914f2b29665112d9cc91b112
Viacheslav Konstantinov <vyacheslav.konstantinov@phystech.edu>
2020-01-14 17:30:54 +0300

    Update Iterator.hpp

--- 08/Iterator.hpp
+++ 08/Iterator.hpp
@@ -2,77 +2,225 @@
 
 template <class T>
 class Iterator
-    : public std::iterator<std::forward_iterator_tag, T>
+    : public std::iterator<std::random_access_iterator_tag, T>
 {
     T* ptr_;
 public:
     
-    Iterator()
+    Iterator();
+    explicit Iterator(T* ptr);
+
+    Iterator(Iterator&& other);
+    Iterator(const Iterator& iter);
+
+    ~Iterator();
+
+    Iterator& operator=(const Iterator& other);
+    Iterator& operator=(Iterator&& other);
+
+    bool operator==(const Iterator<T>& other) const;
+    bool operator!=(const Iterator<T>& other) const;
+
+    bool operator <(const Iterator<T>& other) const;
+    bool operator >(const Iterator<T>& other) const;
+    bool operator<=(const Iterator<T>& other) const;
+    bool operator>=(const Iterator<T>& other) const;
+    
+    T& operator*();//
+
+    Iterator operator+(const size_t) const;
+
+    template<class U>
+    friend Iterator<U> operator+(size_t, const Iterator<U>&);
+
+    Iterator  operator-(const size_t) const;
+    ptrdiff_t operator-(const Iterator&) const;
+
+    Iterator& operator++();
+    Iterator  operator++(int /*dummyInt = 0*/);
+    Iterator& operator--();
+    Iterator  operator--(int /*dummyInt = 0*/);
+
+    Iterator& operator+=(const size_t);
+    Iterator& operator-=(const size_t);
+
+    Iterator& operator[](size_t) const;
+    
+};
+
+template<class T>
+void swap(Iterator<T> r, Iterator<T> s);
+    
+template <class T>
+Iterator<T>::Iterator()
     : ptr_(nullptr)
     {
     }
 
-    explicit Iterator(T* ptr)
-        : ptr_(ptr) {}
-
-    Iterator(Iterator&& other)
+template <class T>
+Iterator<T>::Iterator(T* ptr)
+    : ptr_(ptr) 
     {
-        ptr_ = other.ptr_;
-        other.ptr_ = nullptr;    
     }
 
-    Iterator(const Iterator& iter)
-        : ptr_(iter.ptr_) {}
+template <class T>
+Iterator<T>::Iterator(Iterator&& other)
+{
+    ptr_ = other.ptr_;
+    other.ptr_ = nullptr;    
+}
 
-    ~Iterator() {}
-
-    Iterator& operator=(const Iterator& other)
+template <class T>
+Iterator<T>::Iterator(const Iterator& iter)
+    : ptr_(iter.ptr_) 
     {
-        if(other == *this)
-            return *this;
+    }
 
-        ptr_ = other.ptr_;
+template <class T>
+Iterator<T>::~Iterator() 
+    {
+    }
+
+template <class T>
+Iterator<T>& Iterator<T>::operator=(const Iterator& other)
+{
+    if(other == *this)
         return *this;
-    }
 
-    Iterator& operator=(Iterator&& other)
-    {
-        if(other == *this)
-            return *this;
+    ptr_ = other.ptr_;
+    return *this;
+}
 
-        ptr_ = other.ptr_;
-        other.ptr_ = nullptr;
+template <class T>
+Iterator<T>& Iterator<T>::operator=(Iterator&& other)
+{
+    if(other == *this)
         return *this;
-    }
 
-    bool operator==(const Iterator<T>& other) const
-    {
-        return ptr_ == other.ptr_;
-    }
+    ptr_ = other.ptr_;
+    other.ptr_ = nullptr;
+    return *this;
+}
 
-    bool operator!=(const Iterator<T>& other) const
-    {
-        return !(*this == other);
-    }
+template <class T>
+bool Iterator<T>::operator==(const Iterator<T>& other) const
+{
+    return ptr_ == other.ptr_;
+}
 
-    T& operator*()
-    {
-        return *ptr_;
-    }
+template <class T>
+bool Iterator<T>::operator!=(const Iterator<T>& other) const
+{
+    return !(*this == other);
+}
 
-    Iterator& operator++()
-    {
-        ++ptr_;
-        return *this;
-    }
+template <class T>
+bool Iterator<T>::operator <(const Iterator<T>& other) const
+{
+    return (ptr_ < other.ptr_);
+}
+
+template <class T>
+bool Iterator<T>::operator >(const Iterator<T>& other) const
+{
+    return (ptr_ > other.ptr_);
+}
+
+template <class T>
+bool Iterator<T>::operator<=(const Iterator<T>& other) const
+{
+    return (ptr_ <= other.ptr_);
+}
+
+template <class T>
+bool Iterator<T>::operator>=(const Iterator<T>& other) const
+{
+    return (ptr_ >= other.ptr_);
+}
+
+template <class T>
+T& Iterator<T>::Iterator::operator*()
+{
+    return *ptr_;
+}
+
+template <class T>
+Iterator<T> Iterator<T>::operator+(const size_t n) const
+{
+    Iterator<T> result(ptr_);
+    result.ptr_ = result.ptr_ + n;
+    return result;
+}
+
+template <class T>
+Iterator<T> operator+(size_t n, const Iterator<T>& other)
+{
+    return other + n;
+}
+
+template <class T>
+Iterator<T> Iterator<T>::operator-(const size_t n) const
+{
+    Iterator<T>   result(ptr_);
+    result.ptr_ = result.ptr_ - n;
+    return result;    
+}
+
+template <class T>
+ptrdiff_t Iterator<T>::operator-(const Iterator& other) const
+{
+    return (this->ptr_ - other.ptr_);
+}
+
+template <class T>
+Iterator<T>& Iterator<T>::operator++()
+{
+    ++ptr_;
+    return *this;
+}
     
-    Iterator operator++(int)
-    {
-        Iterator temp = *this;
-        ++*this;
-        return temp;
-    }
-};
+template <class T>
+Iterator<T> Iterator<T>::operator++(int /*dummyInt = 0*/)
+{
+    Iterator temp = *this;
+    ++*this;
+    return temp;
+}
+
+template <class T>
+Iterator<T>& Iterator<T>::operator--()
+{
+    --ptr_;
+    return *this;
+}
+    
+template <class T>
+Iterator<T> Iterator<T>::operator--(int /*dummyInt = 0*/)
+{
+    Iterator temp = *this;
+    --*this;
+    return temp;
+}
+
+template <class T>
+Iterator<T>& Iterator<T>::operator+=(const size_t n)
+{
+    ptr_ = ptr_ + n;
+    return *this;
+}
+
+template <class T>
+Iterator<T>& Iterator<T>::operator-=(const size_t n)
+{
+    ptr_ = ptr_ - n;
+    return *this;
+}
+
+template <class T>
+Iterator<T>& Iterator<T>::operator[](size_t idx) const
+{
+    return (*this + idx);
+}
 
 
 template<class T>
@@ -82,4 +230,4 @@ void swap(Iterator<T> r, Iterator<T> s)
    r = s;
    s = temp;
    return;
-} 
+}
